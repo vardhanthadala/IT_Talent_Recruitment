470fa6390ea741249783e2dec11f2dadf877651c
Vardhan <thadalavardhangoud@gmail.com>
2026-06-22 14:19:05 +0530

    Add premium floating Navbar2 and fix layout imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import Navbar2 from "@/components/Navbar2";
 import LoadingScreen from "@/components/LoadingScreen";
 import Footer from "@/components/Footer";
 
@@ -32,7 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col pt-[72px] overflow-x-clip">
         <LoadingScreen />
-        <Navbar />
+        <Navbar2 />
         {children}
         <Footer />
       </body>
